ad588f4e48595fa4293594bc7b3b766d1065509a
BugRed <o.adrian.rodrigues@gmail.com>
2026-04-30 06:17:04 -0300

    Adição de cards para redes sociais

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
       >
         <ToastProvider>
           <Header />
-          {children}
+          {children} 
           <Footer />
         </ToastProvider>
       </body>
